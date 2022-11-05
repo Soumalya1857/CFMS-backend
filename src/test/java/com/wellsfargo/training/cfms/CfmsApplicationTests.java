af2a13fd1f62afb9125d2b59856995aad9ff18a3
Soumalya Kundu <soumalya@Soumalyas-MacBook-Pro.local>
2022-11-05 14:43:43 +0530

    initial project structure

---
+++ src/test/java/com/wellsfargo/training/cfms/CfmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.training.cfms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
