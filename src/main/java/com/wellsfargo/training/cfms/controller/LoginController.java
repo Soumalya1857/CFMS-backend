603d9d68ca86ae2e46d36323c3ef7d9c0093ec7d
Soumalya Kundu <soumalya@Soumalyas-MacBook-Pro.local>
2022-11-06 11:37:18 +0530

    initial project structure with packages

---
+++ src/main/java/com/wellsfargo/training/cfms/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.wellsfargo.training.cfms.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+}
---
+++ src/main/java/com/wellsfargo/training/cfms/security/ApplicationSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.wellsfargo.training.cfms.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter  {
+	
+
+}
