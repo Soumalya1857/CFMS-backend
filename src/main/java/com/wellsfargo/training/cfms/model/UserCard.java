bd7ac0c69b3d0bb08e76b577f08e48b80dcd6615
Soumalya Kundu <soumalya@Soumalyas-MacBook-Pro.local>
2022-11-07 23:12:07 +0530

    schema updated

--- src/main/java/com/wellsfargo/training/cfms/model/UserCard.java
+++ src/main/java/com/wellsfargo/training/cfms/model/UserCard.java
@@ -1,5 +1,43 @@
 package com.wellsfargo.training.cfms.model;
 
-public class UserCard {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="user_card", uniqueConstraints = @UniqueConstraint(columnNames = "id"))
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class UserCard {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_id")
+	private Long userId; // foreign to user
+	
+	@Column(name = "card_type")
+	private String cardType; // foreign to Card
+	
+	@Column(name = "useable_credit_limit")
+	private Long useableCreditLimit;
+	
+	@Column(name = "is_activated")
+	private boolean isActivated;
+	
 }
