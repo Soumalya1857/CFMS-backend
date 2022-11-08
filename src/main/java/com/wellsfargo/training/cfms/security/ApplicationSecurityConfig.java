85ade23dcb25c3da669226d8e648baa7b67733e3
unknown <syampatnala007@gmail.com>
2022-11-08 16:16:11 +0530

    Added RegisterController working code

--- src/main/java/com/wellsfargo/training/cfms/security/ApplicationSecurityConfig.java
+++ src/main/java/com/wellsfargo/training/cfms/security/ApplicationSecurityConfig.java
@@ -1,17 +1,21 @@
-//package com.wellsfargo.training.cfms.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-////@SuppressWarnings("deprecation")
-//@Configuration
-////@EnableWebSecurity
-//
-//
-//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter  {
-//	
-//	
-//
-//}
+package com.wellsfargo.training.cfms.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@SuppressWarnings("deprecation")
+@Configuration
+//@EnableWebSecurity
+
+
+public class ApplicationSecurityConfig   {
+	 @Bean
+	    public WebSecurityCustomizer webSecurityCustomizer() {
+	        return (web) -> web.ignoring().antMatchers("/samp", "/register","/findUsers");
+	    }
+	}
