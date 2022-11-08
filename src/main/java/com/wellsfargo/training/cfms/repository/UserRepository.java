cce9065e699d0e03bbc58e21e1e66a2c19b098ba
Soumalya Kundu <soumalya@Soumalyas-MacBook-Pro.local>
2022-11-08 09:48:50 +0530

    user service added

--- src/main/java/com/wellsfargo/training/cfms/repository/UserRepository.java
+++ src/main/java/com/wellsfargo/training/cfms/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.wellsfargo.training.cfms.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wellsfargo.training.cfms.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	public Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/wellsfargo/training/cfms/service/UserService.java
@@ -0,0 +1,36 @@
+package com.wellsfargo.training.cfms.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wellsfargo.training.cfms.model.User;
+import com.wellsfargo.training.cfms.repository.UserRepository;
+
+
+
+@Transactional
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User registerUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	public List<User> getAllUser() {
+		return userRepository.findAll();
+	}
+	
+	public Optional<User> loginUser(String email) {
+		
+		return userRepository.findByEmail(email); 
+	}
+
+}
