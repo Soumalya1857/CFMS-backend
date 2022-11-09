6f3173416f1bd80dc415686a64dc60d159285125
Soumalya Kundu <soumalya@Soumalyas-MacBook-Pro.local>
2022-11-09 20:48:39 +0530

    working commit final

--- src/main/java/com/wellsfargo/training/cfms/controller/UserCardController.java
+++ src/main/java/com/wellsfargo/training/cfms/controller/UserCardController.java
@@ -1,5 +1,6 @@
 package com.wellsfargo.training.cfms.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wellsfargo.training.cfms.model.Card;
 import com.wellsfargo.training.cfms.model.UserCard;
+import com.wellsfargo.training.cfms.service.CardService;
 import com.wellsfargo.training.cfms.service.UserCardService;
 
 @RestController
@@ -18,9 +21,26 @@ public class UserCardController {
 	@Autowired
 	UserCardService userCardService;
 	
+	@Autowired
+	CardService cardService;
+	
 	@GetMapping("/getCardDetails")
-	public UserCard getCardDetails(@RequestBody Map<String, Long> body) {
-		return userCardService.getUserCard(body.get("userId")).get();
+	public Map<String, Object> getCardDetails(@RequestBody Map<String, Long> body) {
+		UserCard userCard = userCardService.getUserCard(body.get("userId")).get();
+		
+		Card card = cardService.getCard(userCard.getCardType()).get();
+		
+		return new HashMap <String,Object>() {{
+			put("validity", userCard.getValidity());
+			put("cardType", userCard.getCardType());
+			put("validity", userCard.getValidity());
+			put("Status", "Activated");
+			put("issueDate", userCard.getIssueDate());
+			put("remainingCredit", userCard.getUseableCreditLimit());
+			put("totalCredit", card.getCreditLimit());
+			put("creditUsed", card.getCreditLimit() - userCard.getUseableCreditLimit());
+		}};
+		
 	}
 
 }
